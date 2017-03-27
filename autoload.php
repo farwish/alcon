54e9ed5d1b1d3550c7fb01d3bac7b58457701eeb
farwish <farwish@foxmail.com>
2017-03-27 10:37:29 +0800

    autoload optimize

--- autoload.php
+++ autoload.php
@@ -2,7 +2,7 @@
 /**
  * autoload.php
  *
- * Use it without composer, cant in cli.
+ * Use it without composer.
  *
  * <code>
  * include "/your_path/farwish/alcon/autoload.php";
@@ -16,13 +16,24 @@ class AlconAutoload
     const ALCON_NAMESPACE = 'Alcon\\';
     const ALCON_BASEDIR = 'src/';
     const ALCON_EXT = '.php';
-    
+
     public static function psr4($name)
     {
-        $name = str_replace([self::ALCON_NAMESPACE, '\\'], [self::ALCON_BASEDIR, DIRECTORY_SEPARATOR], $name);
-        $file = __DIR__ . DIRECTORY_SEPARATOR . $name . self::ALCON_EXT;
-        if (! file_exists($file) ) die("File not exists : {$file}\n");
-        include_once $file;
+        // Be sure of include once.
+        static $namespace_map = [];
+
+        // Be sure of this library.
+        if ( (strpos($name, self::ALCON_NAMESPACE) === 0)
+            && ! array_key_exists($name, $namespace_map) ) 
+        {
+            $realname = str_replace([self::ALCON_NAMESPACE, '\\'], [self::ALCON_BASEDIR, DIRECTORY_SEPARATOR], $name);
+            $file = __DIR__ . DIRECTORY_SEPARATOR . $realname . self::ALCON_EXT;
+
+            if ( file_exists($file) ) {
+                include $file;
+                $namespace_map[$name] = $file;
+            }
+        }
     }
 }
 
